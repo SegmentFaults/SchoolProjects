083c42f2e61680808da6acdf7ab6e34e7c7c4b5f
Gentile <gentilm5@miamioh.edu>
2018-02-01 13:42:09 -0500

    Working on a concurrency failure

--- Software Construction/Independent Practice/Concurrency/Concurrency/Concurrency.py
+++ Software Construction/Independent Practice/Concurrency/Concurrency/Concurrency.py
@@ -9,8 +9,8 @@ def only_sleep():
     print("PID: " + str(os.getpid()) + "Process name: " + multiprocessing.process.name + "Thread name " + threading.current_thread().name)
     time.sleep(1)
 
-def compute():
-    print("PID: " + os.getpid() + "Process name: " + multiprocessing.process.name + "Thread name " + threading.current_thread()._name)
+def crunch_numbers():
+    print("PID: " + str(os.getpid()) + "Process name: " + multiprocessing.process.name + "Thread name " + threading.current_thread()._name)
     x = 0
     while x < 10000000:
         x=x+1
@@ -40,3 +40,27 @@ processes = [multiprocessing.Process(target=only_sleep()) for _ in range(NUM_WOR
 end_time = time.time()
  
 print("Parallel time=", end_time - start_time)
+
+
+start_time = time.time()
+for _ in range(NUM_WORKERS):
+    crunch_numbers()
+end_time = time.time()
+ 
+print("Serial time=", end_time - start_time)
+ 
+start_time = time.time()
+threads = [threading.Thread(target=crunch_numbers) for _ in range(NUM_WORKERS)]
+[thread.start() for thread in threads]
+[thread.join() for thread in threads]
+end_time = time.time()
+ 
+print("Threads time=", end_time - start_time)
+ 
+ 
+start_time = time.time()
+processes = [multiprocessing.Process(target=crunch_numbers) for _ in range(NUM_WORKERS)]
+[process.start() for process in processes]
+[process.join() for process in processes]
+end_time = time.time()
+ 
