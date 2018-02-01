62b3af1cab57c17fa3fb0e68cad1103f14d27183
Michael Gentile <gentilm5@miamioh.edu>
2018-02-01 14:41:27 -0500

    Fixed the failure with multithreading application. Needed freeze support added to the beginning of Windows computers. Interpreted correctly on both 2.7.x and 3.5

--- Software Construction/Independent Practice/Concurrency/Concurrency/Concurrency.py
+++ Software Construction/Independent Practice/Concurrency/Concurrency/Concurrency.py
@@ -10,57 +10,56 @@ def only_sleep():
     time.sleep(1)
 
 def crunch_numbers():
-    print("PID: " + str(os.getpid()) + "Process name: " + multiprocessing.process.name + "Thread name " + threading.current_thread()._name)
+    print("PID: " + str(os.getpid()) + "Process name: " + multiprocessing.process.name + "Thread name " + threading.current_thread().name)
     x = 0
     while x < 10000000:
         x=x+1
+if __name__ == '__main__':
+    ## Run tasks serially
+    start_time = time.time()
+    for _ in range(NUM_WORKERS):
+        only_sleep()
+    end_time = time.time()
 
-## Run tasks serially
-start_time = time.time()
-for _ in range(NUM_WORKERS):
-    only_sleep()
-end_time = time.time()
- 
-print("Serial time=", end_time - start_time)
- 
-# Run tasks using threads
-start_time = time.time()
-threads = [threading.Thread(target=only_sleep) for _ in range(NUM_WORKERS)]
-[thread.start() for thread in threads]
-[thread.join() for thread in threads]
-end_time = time.time()
- 
-print("Threads time=", end_time - start_time)
- 
-# Run tasks using processes
-start_time = time.time()
-processes = [multiprocessing.Process(target=only_sleep()) for _ in range(NUM_WORKERS)]
-[process.start() for process in processes]
-[process.join() for process in processes]
-end_time = time.time()
- 
-print("Parallel time=", end_time - start_time)
+    print("Serial time=", end_time - start_time)
+
+    # Run tasks using threads
+    start_time = time.time()
+    threads = [threading.Thread(target=only_sleep) for _ in range(NUM_WORKERS)]
+    [thread.start() for thread in threads]
+    [thread.join() for thread in threads]
+    end_time = time.time()
+
+    print("Threads time=", end_time - start_time)
+
+    # Run tasks using processes
+    start_time = time.time()
+    processes = [multiprocessing.Process(target=only_sleep()) for _ in range(NUM_WORKERS)]
+    [process.start() for process in processes]
+    [process.join() for process in processes]
+    end_time = time.time()
+
+    print("Parallel time=", end_time - start_time)
 
 
-start_time = time.time()
-for _ in range(NUM_WORKERS):
-    crunch_numbers()
-end_time = time.time()
- 
-print("Serial time=", end_time - start_time)
- 
-start_time = time.time()
-threads = [threading.Thread(target=crunch_numbers) for _ in range(NUM_WORKERS)]
-[thread.start() for thread in threads]
-[thread.join() for thread in threads]
-end_time = time.time()
- 
-print("Threads time=", end_time - start_time)
- 
- 
-start_time = time.time()
-processes = [multiprocessing.Process(target=crunch_numbers) for _ in range(NUM_WORKERS)]
-[process.start() for process in processes]
-[process.join() for process in processes]
-end_time = time.time()
- 
+    start_time = time.time()
+    for _ in range(NUM_WORKERS):
+        crunch_numbers()
+    end_time = time.time()
+
+    print("Serial time=", end_time - start_time)
+
+    start_time = time.time()
+    threads = [threading.Thread(target=crunch_numbers) for _ in range(NUM_WORKERS)]
+    [thread.start() for thread in threads]
+    [thread.join() for thread in threads]
+    end_time = time.time()
+
+    print("Threads time=", end_time - start_time)
+
+
+    start_time = time.time()
+    processes = [multiprocessing.Process(target=crunch_numbers) for _ in range(NUM_WORKERS)]
+    [process.start() for process in processes]
+    [process.join() for process in processes]
+    end_time = time.time()
